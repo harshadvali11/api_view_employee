f2e278e00612b9d723a1b0103538d59e80cb68a0
jspmern <jspmern@gmail.com>
2023-11-24 19:43:21 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Employee(models.Model):
+    Ename=models.CharField(max_length=100)
+    Esal=models.IntegerField()
+    Ejob=models.CharField(max_length=100)
